38f73dc2acb1530d4e32a06bd100668c6cb9af28
VP <VP>
2021-11-09 20:37:28 +0200

    Initial Commit

---
+++ BottomCardSwiftUI/BottomCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomCardSwiftUIApp.swift
+//  BottomCardSwiftUI
+//
+//  Created by valentine on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BottomCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
